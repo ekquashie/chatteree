3ab6a211a7288dc63671c3757e7f181098ad930e
Eyram Quashie <eyramkobla.quashie@gmail.com>
2023-07-23 19:32:59 +0000

    fix: build fix

--- src/components/chat/Message.vue
+++ src/components/chat/Message.vue
@@ -1,10 +1,10 @@
 <template>
-  <div v-if="message.type==='text'" :class="message.sender_id !== '15' ? 'bg-[#F1F1F1]' : 'bg-blackPearl text-white'"
+  <div v-if="message?.type==='text'" :class="message?.sender_id !== '15' ? 'bg-[#F1F1F1]' : 'bg-blackPearl text-white'"
        class="px-4 py-2 w-fit max-w-[400px] rounded-full">
-    <p class="font-thin text-[10px] text-right">{{ message.timestamp }}</p>
+    <p class="font-thin text-[10px] text-right">{{ message?.timestamp }}</p>
     <p class="font-thin text-sm">{{ message?.content }}</p>
   </div>
-  <img v-else :src="message.content" width="264" height="164" alt="message-image"/>
+  <img v-else :src="message?.content" width="264" height="164" alt="message-image"/>
 </template>
 
 <script setup lang="ts">
